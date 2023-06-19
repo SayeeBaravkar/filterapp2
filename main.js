b20eccb56862cdc3d1000d89e6672cd7e52f1857
SayeeBaravkar <95957072+SayeeBaravkar@users.noreply.github.com>
2023-06-19 19:34:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload() {
+}
+
+function setup() {
+canvas = createCanvas (300, 300);
+canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+        {
+            console.log(results);
+            noseX = results[0].pose.nose.x - 100
+            noseY = results[0].pose.nose.y - 160
+            console.log("nose x = " + noseX);
+            console.log("nose y = " + noseY);
+        }
+}
+
+
+function draw() {
+}
+
+function take_snapshot(){ save('myFilterImage.png');
+}
